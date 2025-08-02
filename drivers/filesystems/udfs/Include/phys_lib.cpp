5431b976ae7640ddf2d7d24ce8846291dd2218f0
Bryan Kirk <Zero3K@purelymail.com>
2025-08-01 20:17:02 -0400

    Revert "Remove LastLBA changes while keeping IOCTL improvements"
    This reverts commit 0eff2c79260ed7ab01e544b68d4c78caf5f12c94.

--- drivers/filesystems/udfs/Include/phys_lib.cpp
+++ drivers/filesystems/udfs/Include/phys_lib.cpp
@@ -1293,10 +1293,15 @@ UDFGetBlockSize(
                                     DiskGeometryEx.Geometry.TracksPerCylinder *
                                     DiskGeometryEx.Geometry.SectorsPerTrack - 1);
             if (Vcb->LastLBA == 0x7fffffff) {
-                ASSERT(FALSE);
+                // Geometry calculation resulted in problematic value, use reasonable maximum
+                UDFPrint(("UDFGetBlockSize: Geometry calculation overflow, capping LastLBA\n"));
+                Vcb->LastLBA = 0x10000000; // Cap to ~512GB worth of sectors
             }
         } else {
-            ASSERT(FALSE);
+            // Both disk and CDROM geometry IOCTLs failed for non-disk device
+            // This can happen with large VHD files or unsupported devices. Use a reasonable default.
+            UDFPrint(("UDFGetBlockSize: Geometry IOCTLs failed, using default LastLBA\n"));
+            Vcb->LastLBA = 0x10000000; // Default to ~512GB worth of sectors at 2048 bytes/sector
         }
         Vcb->LastPossibleLBA = Vcb->LastLBA;
     }
